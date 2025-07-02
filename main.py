984c8d10e8f816dcef27ceab9b58fe40a1560a39
Varun Menon <varunmenon2004@gmail.com>
2025-07-02 18:58:18 -0400

    finished text extraction and text-to-audio

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ import PyPDF2
 from tkinter.filedialog import *
 
 book = askopenfilename() # asks  to open a file's name
-pdfreader = PyPDF2.PdfFileReader(book) # assigns book to pdf reader
+pdfreader = PyPDF2.PdfFileReader(book) # assigns book to pdf 
+pages = len(pdfreader.pages) # returns num of pages in pdf
+
+# this section reads all the data from the collected PDF
+
+for num in range(0,pages): # for loop from first page to last page
+    page = pdfreader.pages[num] # makes each page be returned as an object
+    text = page.extract_text() # extracts all the plain text from the page
+    player = pyttsx3.init() 
+    player.say(text)
+    player.runAndWait()
 
