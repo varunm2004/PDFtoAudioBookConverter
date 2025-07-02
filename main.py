630c207529f16cad4cd11e926efc9b4e8a4a156b
Varun Menon <varunmenon2004@gmail.com>
2025-07-02 19:07:40 -0400

    replaced deprecated functions

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pyttsx3
 import PyPDF2
+import pdfreader
 from tkinter.filedialog import *
 
-book = askopenfilename() # asks  to open a file's name
-pdfreader = PyPDF2.PdfFileReader(book) # assigns book to pdf 
-pages = len(pdfreader.pages) # returns num of pages in pdf
+book = askopenfilename() # requests user to give a book file
 
-# this section reads all the data from the collected PDF
+pdfreader = PyPDF2.PdfReader(book) # assigns book object a pdf reader
+
+pages = len(pdfreader.pages) # returns number of pages in the pdf
+
 
 for num in range(0,pages): # for loop from first page to last page
     page = pdfreader.pages[num] # makes each page be returned as an object
